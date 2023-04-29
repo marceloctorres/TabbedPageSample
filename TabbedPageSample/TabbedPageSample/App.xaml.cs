b313525bf83a4a86c9f76a0053dd2144766c3698
Marcelo César Torres <mtorres@esri.co>
2023-04-29 17:08:35 -0500

    Agregar archivos de proyecto.

---
+++ TabbedPageSample/TabbedPageSample/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.Forms;
+
+namespace TabbedPageSample {
+  public partial class App : Application {
+    public App() {
+      InitializeComponent();
+
+      MainPage = new MainTabbedPage();
+    }
+
+    protected override void OnStart() {
+      // Method intentionally left empty.
+    }
+
+    protected override void OnSleep() {
+      // Method intentionally left empty.
+    }
+
+    protected override void OnResume() {
+      // Method intentionally left empty.
+    }
+  }
+}
---
+++ TabbedPageSample/TabbedPageSample/MainTabbedPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TabbedPageSample {
+  [XamlCompilation(XamlCompilationOptions.Compile)]
+  public partial class MainTabbedPage : TabbedPage {
+    public MainTabbedPage() {
+      InitializeComponent();
+    }
+  }
+}
